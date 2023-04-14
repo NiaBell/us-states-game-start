580bbde589d1fcac56884e661f585f176e6d650c
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2023-04-13 20:59:10 -0400

    1. Converted the guess to title case 2. check if th eguess is among th 50 stares 3. write correct guess on the map

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -10,13 +10,31 @@ IMAGE = "blank_states_img.gif"
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
 
+writing = turtle.Turtle()
+
+
 with open("50_states.csv") as dataFile:
     data = pandas.read_csv(dataFile)
 
+states = data["state"]
+states = states.tolist()
 
-states = data.to_dict()
-print(states)
+# state_x = data["state"]
+# states = states.tolist()
+#
+# state_y = data["state"]
+# states = states.tolist()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another State?")
-print(answer_state)
 
+
+
+answer_state = (screen.textinput(title="Guess the State", prompt="What's another State?")).title()
+#
+#
+if answer_state in states:
+    writing.penup()
+    writing.goto(139,-77)
+    writing.write(answer_state)
+
+
+answer_state = (screen.textinput(title="Guess the State", prompt="What's another State?")).title()
