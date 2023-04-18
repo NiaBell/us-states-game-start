168c7745edd0e5727288064e369b1467a0ef70d7
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2023-04-17 20:38:02 -0400

    1. made x & y coordinate variable by isolation row and series. 2. Made an exit command to end game. 3. made a csv file upon the completion of game of unguessed states for user to review .

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,40 +1,41 @@
 import turtle
-from turtle import Screen
 import pandas
 
-
-screen = Screen()
-
+screen = turtle.Screen()
 screen.title("US States Game")
 IMAGE = "blank_states_img.gif"
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
 
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
 writing = turtle.Turtle()
 
-
-with open("50_states.csv") as dataFile:
-    data = pandas.read_csv(dataFile)
+guessed_states=[]
+score = 0
 
 states = data["state"]
 states = states.tolist()
 
-# state_x = data["state"]
-# states = states.tolist()
-#
-# state_y = data["state"]
-# states = states.tolist()
+while len(guessed_states) < 50:
+    answer_state = (screen.textinput(title= f"{len(guessed_states)} Correct States", prompt="What's another State?")).title()
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
 
+        break
+    if answer_state in states:
+        writing.penup()
+        state_data = data[data.state == answer_state]
+        writing.goto(int(state_data.x),int(state_data.y))
+        writing.write(answer_state)
+        guessed_states.append(answer_state)
+        score += 1
 
-
-answer_state = (screen.textinput(title="Guess the State", prompt="What's another State?")).title()
-#
-#
-if answer_state in states:
-    writing.penup()
-    writing.goto(139,-77)
-    writing.write(answer_state)
-
-
-answer_state = (screen.textinput(title="Guess the State", prompt="What's another State?")).title()
+#states to learn.csv
